e56f68982bc5f7d5d9ffb4b9ab96e0e7cf6ae927
wanjun <wanjunlan@gmail.com>
2024-07-05 15:31:33 -0700

    designed the Todo list card content structure rendering Todo title complete status toggle button and delete button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -14,6 +14,21 @@
         <el-input v-model="newTodo" placeholder="New Todo"></el-input>
         <el-button type="primary" @click="addTodo">Add</el-button>
       </div>
+      <div v-if="todos.length" class="todo-list">
+        <el-card v-for="todo in todos" :key="todo.id" class="todo-item">
+          <div class="todo-item-actions">
+            <div class="todo-item-title"> {{todo.title}}</div>
+            <el-button class="todo-button" @click="toggleComplete(todo)"
+                        :type="todo.completed ? 'success': 'info'">
+              {{todo.completed ? 'Completed': 'Not Complete'}}
+            </el-button>
+            <el-button type="danger" @click="deleteTodo(todo)">
+              Delete
+            </el-button>
+          </div>
+        </el-card>
+
+      </div>
     </el-card>
   </div>
 
